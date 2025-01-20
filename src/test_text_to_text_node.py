11b042a78b342de031d124a4c5e60bb2f201b75f
Levi Sanchez <tkd.lsanchez@gmail.com>
2025-01-20 15:25:39 -0700

    Sets up the 'text_to_text_node' function.

---
+++ src/test_text_to_text_node.py
@@ -0,0 +1,52 @@
+import unittest
+from textnode import TextType, TextNode
+from text_to_text_node import text_to_textnodes
+#
+class TestTextToTextNode(unittest.TestCase):
+    def test_sanity_check(self):
+        self.assertEqual(2, 2)
+    #
+    def test_basic_situation(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        output =  [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        something = text_to_textnodes(text)
+        self.assertListEqual(something, output)
+    #
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+
+
+#
+if __name__ == "__main__":
+    unittest.main()
+
+ 
--- src/text_to_text_node.py
+++ src/text_to_text_node.py
@@ -1,4 +1,18 @@
 from textnode import TextNode, TextType
+from splitter import split_nodes_delimiter
+from image_extractor import split_nodes_image
+from link_extractor import split_nodes_link
 
+"""
+"""
 def text_to_textnodes(text):
-    pass 
+    nodes = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_delimiter([nodes], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    #   
+    return new_nodes
+
+#
